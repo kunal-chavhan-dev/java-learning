5437bcc4f469b86819890392198b51db98e824c6
Kunal Chavhan <chavhankunal172@gmail.com>
2026-06-13 08:49:30 +0530

    traffic light simulatordone using scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Hello GitHub!")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        // CONDITION START
+        if(button == 1) {
+            System.out.println("Stop! The light is RED");
+        }
+        else if(button == 2 ) {
+            System.out.println("Wait! The light is YELLOW");
+        }
+        else if(button == 3 ) {
+            System.out.println("Go! The light is GREEN");
+        }
+        else{
+            System.out.println("Invalid Button");
+        }
     }
 }
