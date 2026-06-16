ed54ddfff31c33171034807db7b5936a5b990947
Kunal Chavhan <chavhankunal172@gmail.com>
2026-06-16 10:22:58 +0530

    Updated pattern programs

--- src/Cwk_06_DiamondPattern.java
+++ src/Cwk_06_DiamondPattern.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
+package com.myprojects.patterns;
 public class Cwk_06_DiamondPattern {
+        public static void main(String[] args) {
+            int n = 5;
+
+            // Upper Part of the Diamond
+            for (int i = 1; i <= n; i++) {
+                // Print leading spaces
+                for (int j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                // Print numbers
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(i + " ");
+                }
+                // Move to the next line
+                System.out.println();
+            }
+
+            // Lower Part of the Diamond
+            for (int i = n - 1; i >= 1; i--) {
+                // Print leading spaces
+                for (int j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(i + " ");
+                }
+                System.out.println();
+            }
+        }
+
 }
--- src/Cwk_08_RightTriangle.java
+++ src/Cwk_08_RightTriangle.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
-
 public class Cwk_08_RightTriangle {
-}
+        public static void main(String[] args) {
+            int n = 5;
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(i + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println(" --- DIFFERENT PATTERN --");
+
+
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println(" --- COMBINE TRIANGLE--- ");
+
+
+                    // Part 1: Increasing Triangle (1 to 5)
+                    for (int i = 1; i <= n; i++) {
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print(j + " ");
+                        }
+                        System.out.println();
+                    }
+
+                    // Part 2: Decreasing Triangle (4 to 1)
+                    for (int i = n - 1; i >= 1; i--) {
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print(j + " ");
+                        }
+                        System.out.println();
+                    }
+
+            }
+
+
+        }
+
+
