ebd428eb88c1c2158ba9c036659d1b3b314b4474
Kunal Chavhan <chavhankunal172@gmail.com>
2026-06-21 20:20:26 +0530

    Added MaxFinder program logic

---
+++ src/Cwk_15_MaxFinder.java
@@ -0,0 +1,21 @@
+public class Cwk_15_MaxFinder {
+        public static void main(String[] args) {
+            int[] numbers = {23, 67, 12, 89, 5, 41};
+
+            int max = numbers[0];
+            int min = numbers[0];
+
+            for (int i = 1; i < numbers.length; i++) {
+                if (numbers[i] > max) {
+                    max = numbers[i];
+                }
+                if (numbers[i] < min) {
+                    min = numbers[i];
+                }
+            }
+
+            System.out.println("Maximum: " + max);
+            System.out.println("Minimum: " + min);
+        }
+
+}
