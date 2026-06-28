879cede4674767e4c1c7703ce5eefaaedbc259b5
Kunal Chavhan <chavhankunal172@gmail.com>
2026-06-28 19:45:48 +0530

    Transpose program

---
+++ src/Cwk_23_Transpose.java
@@ -0,0 +1,26 @@
+ public class Cwk_23_Transpose {
+        public static void main(String[] args) {
+            int[][] matrix = {
+                    {1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}
+            };
+
+            int[][] transpose = new int[3][3];
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    transpose[j][i] = matrix[i][j]; // row <-> col swap
+                }
+            }
+
+            System.out.println("Transposed Matrix:");
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(transpose[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+}
