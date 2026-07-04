83720f912351ad0bad0eaceae70ba0c960e35895
Kunal Chavhan <chavhankunal172@gmail.com>
2026-07-04 19:55:18 +0530

    Added StringExample

---
+++ src/Cwk_31_StringExample.java
@@ -0,0 +1,13 @@
+public class Cwk_31_StringExample {
+        public static void main(String[] args) {
+            // String Literal (Recommended)
+            String greeting = "Hello, World!";
+
+            // Using the 'new' keyword
+            String name = new String("Java Learner");
+
+            System.out.println(greeting);
+            System.out.println(name);
+        }
+    }
+
