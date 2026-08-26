faeea4ecef503eb6173f5f950dec48b4f3b84820
Kunal-cse12 <chavhankunal172@gmail.com>
2026-08-26 20:07:32 +0530

    Added Array Example

--- src/Cwk_007_LarSmaDemo.java
+++ src/Cwk_007_LarSmaDemo.java
@@ -1,16 +1,5 @@
 public class Cwk_007_LarSmaDemo {
     public static void main(String[] args) {
-        int arr[] = {20,30,50,60,70,66};
-        int n = arr.length;
-        int max = arr[0];
-        int min = arr[0];
-
-        for(int i = 0; i < n; i++) {
-            if(arr[i] > max) max = arr[i];
-                    if(arr[i] < min ) min = arr[i];
-        }
-        System.out.println("max = " + max );
-        System.out.println("min = " + min);
 
     }
 }
