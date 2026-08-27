32bad909892b5be9bde1e0998e70168aa04a32ba
Kunal-cse12 <chavhankunal172@gmail.com>
2026-08-27 16:29:58 +0530

    Added String Example

---
+++ src/Cwk_41_StringBasicExample.java
@@ -0,0 +1,16 @@
+public class Cwk_41_StringBasicExample {
+        public static void main(String[] args) {
+            String s = "Kunal";
+
+            // 1. Length nikalna
+            System.out.println("Length: " + s.length()); // Output: 5
+
+            // 2. Character extract karna
+            System.out.println("Char at index 2: " + s.charAt(2)); // Output: n
+
+            // 3. Lowercase aur Uppercase
+            System.out.println("Lowercase: " + s.toLowerCase()); // Output: kunal
+            System.out.println("Uppercase: " + s.toUpperCase()); // Output: KUNAL
+        }
+
+}
